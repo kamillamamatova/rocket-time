1b098ce0451dafb48ebd2befba9dc9084393df94
et <eileentomy@gmail.com>
2025-10-26 01:33:33 -0400

    date formatted backend

--- rocketTime/apps/server/src/services/addLog.js
+++ rocketTime/apps/server/src/services/addLog.js
@@ -3,10 +3,12 @@ import db from './db.js'; // import database
 //get one user' log
 export async function addTimeLogs(logbody){
     try{
+        const date=new Date(logbody.date);
+        const mysqldate=date.toISOString().slice(0,19).reaplce("T", " ");
         const result = await db.query(
             ` INSERT INTO timelogs(user_id, goal_id, date, duration_hr, category, title)
       VALUES (?, ?, ?, ?, ?, ?)`,
-            [logbody.user_id, logbody.goal_id, logbody.date, 
+            [logbody.user_id, logbody.goal_id, mysqldate, 
                 logbody.duration_hr, logbody.category, 
                 logbody.title]
         );
